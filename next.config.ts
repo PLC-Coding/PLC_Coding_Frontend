33582fb49358cebf021deb411b0e9508f38a6d3a
Travis-Ashcraft <tcashcraft@tstc.edu>
2025-05-07 08:21:33 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
-const repoName = "PLC_Coding_Frontend";
 
 const nextConfig: NextConfig = {
     output: "export",
     images: {
         unoptimized: true,
-    },
-    basePath: `/${repoName}`,
-    assetPrefix: `/${repoName}/`,
+    }
 };
 
 export default nextConfig;
